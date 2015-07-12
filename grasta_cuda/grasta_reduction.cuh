cfbfe386dce3afaa9098c5b5fff42bea5c2d36d2
Steve Roudebush <roudy@umich.edu>
2015-07-12 00:13:40 -0400

    begin reduction kernel

---
+++ grasta_cuda/grasta_reduction.cuh
@@ -0,0 +1,11 @@
+#ifndef REDUCTION_H
+#define REDUCTION_H
+
+#include "grasta_cuda_util.cuh"
+
+template <unsigned int blockSize>
+__global__ void simple_reduce(float *g_idata, float *g_odata, unsigned int n)
+{
+
+}
+#endif //REDUCTION_H
